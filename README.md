a067d0b9a1cfd59139fc54df903e7c95ff8953d5
Ian Castleden <ian.castleden@gmail.com>
2022-09-25 16:39:38 +0800

    added delegates

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 
 # Svelte + Flask + ESbuild Template
 
+The reason I like this setup is that I can create server side
+templates using jinja2, generate server side html fragments using
+jinja2 macros in the [HTML over the web way](https://hotwired.dev).
+So creation of static and semi-static html pages is simple.
+And *also* create an "app" with multiple target points per page, viz `main.js`:
+
+```javascript
+import './app.css'
+import App from './App.svelte'
+import AnotherApp from './AnotherApp.svelte'
+// target two places in the jinja template to run
+// our apps.
+export const app = new App({
+  target: document.getElementById('app')
+})
+export const another_app = new AnotherApp({
+  target: document.getElementById('another-app')
+})
+
+```
+This, I think, gives me the best of both worlds.
+
+Also `esbuild` allows me to create build scripts (see `svelte-build/build.main.mjs`)
+to create multiple bundles.
+
+## To instantiate
+
 cd to this directory and run:
 
 ```bash
---
+++ src/lib/delegates.ts
@@ -0,0 +1,67 @@
+// very much simplified version of jQuery delegate
+// https://github.com/jquery/jquery/blob/main/src/event.js#L354
+
+
+export function aselem(e: EventTarget): HTMLElement {
+    return e as HTMLElement;
+}
+export class Delegate {
+    readonly handlers: ((e: MouseEvent) => any)[];
+    readonly selector: string;
+
+    constructor(selector: string) {
+        this.selector = selector;
+        this.handlers = [];
+        document.documentElement.querySelector(selector);
+    }
+
+    add(handler: (e: MouseEvent) => any) {
+        this.handlers.push(handler);
+        return this;
+    }
+    // on:click={delegate.on}
+    on = (e: MouseEvent) => {
+        if (e.currentTarget === null) return
+        const elems = aselem(e.currentTarget).querySelectorAll(
+            this.selector
+        );
+        if (elems === null) return;
+        for (let i = 0; i < elems.length; i++) {
+            if (elems[i] === e.target) {
+                this.handlers.forEach((handler) => {
+                    const res = handler.apply(e.target, [e]);
+                    if (res === false) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }
+                });
+                break;
+            }
+        }
+    };
+}
+// const func = delegate('.class ol li', (e) => {
+//     /* do something with e.target */
+// })
+// <div on:click={func} >
+//  {@html html}
+// </div>
+export function delegate(selector: string, handler: (e: MouseEvent) => any) {
+    // check that selector is valid
+    document.documentElement.querySelector(selector);
+    return function (e: MouseEvent) {
+        if (e.currentTarget === null) return
+        const elems = aselem(e.currentTarget).querySelectorAll(selector);
+        if (elems === null) return;
+        for (let i = 0; i < elems.length; i++) {
+            if (elems[i] === e.target) {
+                const res = handler.apply(e.target, arguments);
+                if (res === false) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+                break;
+            }
+        }
+    };
+}
