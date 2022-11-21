4becddfef0498e6a20c2be72e9c231462de2aca8
arabidopsis <ian.castleden@gmail.com>
2022-11-21 08:24:03 +0800

    select

--- src/lib/bootstrap/Select.svelte
+++ src/lib/bootstrap/Select.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { createEventDispatcher } from "svelte";
   import Tags from "bootstrap5-tags";
   export let options: string[] = [];
   let selected: string[] = [];
-  let select: HTMLSelectElement;
+
   const dispatch = createEventDispatcher();
-  onMount(() => {
-    const tags = new Tags(select);
-    return () => tags.dispose();
-  });
+
+  function tags(node: HTMLSelectElement) {
+    let tags = new Tags(node);
+    return {
+      destroy: () => {
+        tags.dispose();
+      },
+    };
+  }
   $: dispatch("selected", selected);
 </script>
 
 see <a href="https://github.com/lekoala/bootstrap5-tags">repo</a>
 <select
-  bind:this={select}
   class="form-select"
   size="3"
   multiple
-  bind:value={selected}
+  use:tags
   data-badge-style="info"
 >
   <option disabled hidden value="">Choose a tag...</option>
