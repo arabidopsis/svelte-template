62eb6c2ca518be3ddbda958f4842d1bb5c09c057
arabidopsis <ian.castleden@gmail.com>
2022-11-28 17:14:34 +0800

    ensure lib

---
+++ src/lib/EnsureLib.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+	export let src: string;
+	export let loaded = false;
+	const dispatch = createEventDispatcher();
+
+	function onload() {
+		loaded = true;
+		dispatch("load", src);
+	}
+</script>
+
+<!--
+	<EnsureLib src="https://cdn.plot.ly/plotly-2.16.1.min.js">
+		<Plotly />
+		<Plotly />
+	</EnsureLib>
+-->
+<svelte:head>
+	<script {src} on:load={onload}></script>
+</svelte:head>
+
+{#if loaded}
+	<slot />
+{/if}
