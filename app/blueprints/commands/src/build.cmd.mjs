e4b88182f34e1f1dcac0f05ca98a9e99a65e914f
arabidopsis <ian.castleden@gmail.com>
2022-12-05 10:46:01 +0800

    removed blueprints

--- app/blueprints/commands/src/build.cmd.mjs
+++
@@ -1,7 +0,0 @@
-import build from '../../../../svelte-build/build.mjs';
-// npx node app/blueprints/commands/src/build.cmd.mjs -- --watch
-const result = await build({
-    entryPoints: ["app/blueprints/commands/src/cmd.js"],
-    outdir: "app/blueprints/commands/static/assets"
-
-})
--- app/blueprints/delay/src/delay.js
+++
@@ -1,5 +0,0 @@
-
-import Delay from './Delay.svelte'
-const app = new Delay({
-  target: document.getElementById('app')
-})
