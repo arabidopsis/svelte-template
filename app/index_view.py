112865e90aa47da845a817283a5b44ba100af491
arabidopsis <ian.castleden@gmail.com>
2022-12-05 17:46:29 +0800

    favicon

--- app/index_view.py
+++ app/index_view.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from flask import Blueprint
 from flask import Flask
 from flask import render_template
+from flask import Response
+from flask import send_from_directory
 
 
 view = Blueprint("view", __name__)
@@ -11,6 +15,11 @@ view = Blueprint("view", __name__)
 @view.route("/")
 def index() -> str:
     return render_template("index.html")
+
+
+@view.route("/favicon.ico")
+def favicon() -> Response:
+    return send_from_directory(Path("static", "img"), "svelte-blue.svg")
 
 
 def init_app(app: Flask, url_prefix: str = "/") -> None:
