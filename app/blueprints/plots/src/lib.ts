f36a763c1fcaa8e203a77f8cc35e8df993a6c529
arabidopsis <ian.castleden@gmail.com>
2023-04-03 17:10:53 +0800

    comments

--- app/blueprints/plots/src/lib.ts
+++ app/blueprints/plots/src/lib.ts
@@ -1,6 +1,8 @@
 type ArrayOf<Type> = {
     [Property in keyof Type]: Type[Property][];
 };
+// array of type T to an object of lists of attributes
+// e.g. unzip([{a:1, b:2}, {a:3,b:4}], 'a', 'b') => {a:[1,3], b:[2,4]}
 export const unzip = <T, K extends keyof T>(arr: T[], ...keys: K[]): Partial<ArrayOf<T>> => {
     const ret: Partial<ArrayOf<T>> = {};
 
